1274ccbd3235aec9493968d11cf0057b9007ad93
Jack Salloway <jacksalloway@hotmail.com>
2022-09-06 09:44:18 +0100

    Add response messages for register route

--- src/Components/LoginWrapper.jsx/LoginWrapper.jsx
+++ src/Components/LoginWrapper.jsx/LoginWrapper.jsx
@@ -11,6 +11,7 @@ const LoginWrapper = (props) => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loginResMsg, setLoginResMes] = useState(null);
 
     useEffect(() => {
         // Reset states when newUser state is switched
@@ -78,9 +79,10 @@ const LoginWrapper = (props) => {
                 </form>
                 <button
                     className="user-form-button"
-                    id="create-account-button"
+                    id="no-account-button"
                     onClick={() => {
                         setNewUser(true);
+                        setLoginResMes(null);
                     }}
                 >
                     Don't have an account?
@@ -102,59 +104,90 @@ const LoginWrapper = (props) => {
             },
             // withCredentials: true,
             url: "https://seros-interactive-map.herokuapp.com/register",
+        }).then((response) => {
+            // Sending a 401 or 409 status response crashed the app and stopped it from working, so unsure how to do this next part efficiently
+            if (
+                response.data ===
+                "Username already exists. Please choose a different one."
+            ) {
+                setLoginResMes(response.data);
+                return;
+            }
+            if (
+                response.data ===
+                "Email is already in use. Please use a different one."
+            ) {
+                setLoginResMes(response.data);
+                return;
+            }
+
+            if (response.data === "User created! Please login!") {
+                setNewUser(false);
+                setLoginResMes(response.data);
+                return;
+            }
         });
     };
 
     const renderCreateUserForm = () => {
         return (
             <>
-                <h2>Create an account!</h2>
-                <p>Create a new account!</p>
                 <form onSubmit={createUser} className="user-form-inputs">
-                    <label htmlFor="login-name">
-                        Username
-                        <input
-                            type="text"
-                            id="login-name"
-                            required
-                            placeholder="username"
-                            value={username}
-                            onChange={({ target }) => {
-                                setUsername(target.value);
-                            }}
-                        />
-                    </label>
-                    <label htmlFor="login-email">
-                        Email
-                        <input
-                            type="email"
-                            id="login_email"
-                            required
-                            placeholder="email"
-                            value={email}
-                            onChange={({ target }) => {
-                                setEmail(target.value);
-                            }}
-                        />
-                    </label>
-                    <label htmlFor="login-password">
-                        Password
-                        <input
-                            type="password"
-                            id="login-password"
-                            required
-                            placeholder="password"
-                            value={password}
-                            onChange={({ target }) => {
-                                setPassword(target.value);
-                            }}
-                        />
-                    </label>
-                    <button>Create account!</button>
+                    <fieldset>
+                        <legend>Create an account!</legend>
+                        <label htmlFor="login-name">
+                            Username
+                            <input
+                                type="text"
+                                id="login-name"
+                                required
+                                placeholder="username"
+                                value={username}
+                                onChange={({ target }) => {
+                                    setUsername(target.value);
+                                }}
+                            />
+                        </label>
+                        <label htmlFor="login-email">
+                            Email
+                            <input
+                                type="email"
+                                id="login_email"
+                                required
+                                placeholder="email"
+                                value={email}
+                                onChange={({ target }) => {
+                                    setEmail(target.value);
+                                }}
+                            />
+                        </label>
+                        <label htmlFor="login-password">
+                            Password
+                            <input
+                                type="password"
+                                id="login-password"
+                                required
+                                placeholder="password"
+                                value={password}
+                                onChange={({ target }) => {
+                                    setPassword(target.value);
+                                }}
+                            />
+                        </label>
+                        <button
+                            className="user-form-button"
+                            id="create-account-button"
+                        >
+                            Create account!
+                        </button>
+                    </fieldset>
                 </form>
                 <button
+                    className="user-form-button"
+                    id="create-account-button"
                     onClick={() => {
                         setNewUser(false);
+                        setLoginResMes(null);
                     }}
                 >
                     Already have an account?
@@ -165,11 +198,10 @@ const LoginWrapper = (props) => {
 
     return (
         <div id="homepage-banner">
+            {loginResMsg === null ? null : (
+                <p className="login-response-message">{loginResMsg}</p>
+            )}
             {newUser === false ? renderLoginForm() : renderCreateUserForm()}
-            {/* <Link to={'/login'}>Login!</Link> */}
-            <br />
-
-            {/* <Link to={"/interactive-map"}>Continue as a guest!</Link> */}
         </div>
     );
 };
