601f83e7ab9ba05e2bc06bcea029d48525d70307
Jack Salloway <jacksalloway@hotmail.com>
2022-09-21 12:00:11 +0100

    Re-order element layout

--- src/Components/QuestList/QuestListWrapper.jsx
+++ src/Components/QuestList/QuestListWrapper.jsx
@@ -6,11 +6,9 @@ import "./QuestListWrapper.css";
 import QuestListNotes from "./QuestListNotes";
 
 const QuestListWrapper = (props) => {
-    const { serosLocations, setLocationNotes, serosQuests, showTooltip, map } =
-        props;
+    const { serosLocations, setLocationNotes, serosQuests, map } = props;
 
     const [questList, SetQuestList] = useState(serosQuests);
-    // const [showTooltip, setShowTooltip] = useState(false);
     const [searchValue, setSearchValue] = useState("");
 
     // Filter the quest list whenever the user edits the searchValue state
@@ -50,20 +48,6 @@ const QuestListWrapper = (props) => {
 
     return (
         <div id="quest-list-wrapper">
-            {/* Removed the tooltip toggle logic for now, plan to implement it again at a later date within in this and other wrapper components */}
-            <div id="quest-list-tooltip">
-                <p>
-                    <span className="span-color-green">Completed</span> quests
-                    are colored
-                    <span className="span-color-green"> green</span>.
-                </p>
-                <p>
-                    <span className="span-color-red">Incomplete</span> quests
-                    are colored
-                    <span className="span-color-red"> red</span>.
-                </p>
-            </div>
-
             <div id="quest-list-wrapper-header">
                 <input
                     type="text"
@@ -81,6 +65,19 @@ const QuestListWrapper = (props) => {
                     setLocationNotes={setLocationNotes}
                 />
             ))}
+            {/* Removed the tooltip toggle logic for now, plan to implement it again at a later date within in this and other wrapper components */}
+            <div id="quest-list-tooltip">
+                <p>
+                    <span className="span-color-green">Completed</span> quests
+                    are colored
+                    <span className="span-color-green"> green</span>.
+                </p>
+                <p>
+                    <span className="span-color-red">Incomplete</span> quests
+                    are colored
+                    <span className="span-color-red"> red</span>.
+                </p>
+            </div>
         </div>
     );
 };
