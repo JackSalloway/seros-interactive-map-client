529ae8cd51801e64057c96f28528dede3137d957
Jack Salloway <jacksalloway@hotmail.com>
2022-10-04 09:59:50 +0100

    Represent NPCs that do not have assigned locations

--- src/Components/NPCList/NPCListNotes.jsx
+++ src/Components/NPCList/NPCListNotes.jsx
@@ -31,7 +31,11 @@ const NPCListNotes = (props) => {
     if (selected === false) {
         return (
             <div className="npc-list-notes-individual npc-list-notes-individual-header">
-                <p>{he.decode(npc.name)}</p>
+                {npc.associated_locations.length === 0 ? (
+                    <p>*{he.decode(npc.name)}*</p>
+                ) : (
+                    <p>{he.decode(npc.name)}</p>
+                )}
                 {expandDownChevron}
             </div>
         );
@@ -43,30 +47,38 @@ const NPCListNotes = (props) => {
 
     // Create a list of associated locations and a button that takes the user to that location
     const npcAssociatedLocationsList = () => {
-        return npc.associated_locations.map((location) => (
-            <div className="npc-list-notes-locations" key={location._id}>
+        if (npc.associated_locations.length === 0) {
+            return (
                 <div className="npc-list-notes-locations-name">
-                    {he.decode(location.name)}
+                    This NPC has no associated locations...
                 </div>
-                <button
-                    className="npc-list-notes-locations-lat-lng"
-                    onClick={() => {
-                        if (map.current.getZoom() === 5) {
-                            map.current.flyTo(location.latlng);
-                        } else {
-                            map.current.setView(location.latlng, 5);
-                        }
-                        setLocationNotes(
-                            serosLocations
-                                .map((serosLocation) => serosLocation._id)
-                                .indexOf(location._id)
-                        );
-                    }}
-                >
-                    Jump to location!
-                </button>
-            </div>
-        ));
+            );
+        } else {
+            return npc.associated_locations.map((location) => (
+                <div className="npc-list-notes-locations" key={location._id}>
+                    <div className="npc-list-notes-locations-name">
+                        {he.decode(location.name)}
+                    </div>
+                    <button
+                        className="npc-list-notes-locations-lat-lng"
+                        onClick={() => {
+                            if (map.current.getZoom() === 5) {
+                                map.current.flyTo(location.latlng);
+                            } else {
+                                map.current.setView(location.latlng, 5);
+                            }
+                            setLocationNotes(
+                                serosLocations
+                                    .map((serosLocation) => serosLocation._id)
+                                    .indexOf(location._id)
+                            );
+                        }}
+                    >
+                        Jump to location!
+                    </button>
+                </div>
+            ));
+        }
     };
 
     if (selected === true) {
