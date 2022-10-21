7ca6ad10a46cb1c2e53f8cd2b1237bd0d9a609d6
Jack Salloway <jacksalloway@hotmail.com>
2022-10-21 09:25:17 +0100

    Remove redundant code

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -12,42 +12,6 @@ const Dashboard = (props) => {
 
     const [scale, setScale] = useState(1);
 
-    // console.log(campaigns);
-
-    // const campaignSelectBanner = (name, description, campaignID) => {
-    //     return (
-    //         <div
-    //             className="dashboard-banner"
-    //             onMouseEnter={() => {
-    //                 setScale(1.01);
-    //             }}
-    //             onMouseLeave={() => {
-    //                 setScale(1);
-    //             }}
-    //             style={{ transform: `scale(${scale})` }}
-    //         >
-    //             <div className="dashboard-banner-text">
-    //                 <h2>{name}</h2>
-    //                 <p>{description}</p>
-    //             </div>
-    //             <div
-    //                 className="dashboard-banner-image"
-    //                 style={{
-    //                     backgroundImage: `url(./images/Seros_FotE_banner.png)`,
-    //                 }}
-    //             >
-    //                 <button
-    //                     onClick={() => {
-    //                         setCampaign(campaignID);
-    //                     }}
-    //                 >
-    //                     Select campaign!
-    //                 </button>
-    //             </div>
-    //         </div>
-    //     );
-    // };
-
     const loginPrompt = () => {
         return (
             <div id="dashboard-login-prompt">
