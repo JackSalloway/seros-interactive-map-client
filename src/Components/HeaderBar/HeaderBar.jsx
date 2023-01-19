486befb29a41a903aa4b6ea9b17f42550f74c7fd
Jack Salloway <jacksalloway@hotmail.com>
2023-01-19 14:00:28 +0000

    Remove redundant import

--- src/Components/HeaderBar/HeaderBar.jsx
+++ src/Components/HeaderBar/HeaderBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 
