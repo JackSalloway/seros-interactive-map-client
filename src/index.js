397fa538701cb4d67fb826900d8e42a106eeaa81
Jack Salloway <jacksalloway@hotmail.com>
2023-02-13 10:06:18 +0000

    Add BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import AppRouter from "./AppRouter";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // disabled React.StrictMode as is caused issues due to calling routes twice through useEffects
-root.render(<App />);
+// The issue mentioned above is the application calls the startup route twice when it loads. Still not sure why it does this, but have enabled strict mode for reasons mentioned below
 
-// root.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
+// root.render(<App />);
+
+// Re-enabled React.StrictMode to try and implement React-Router
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
 // if (module.hot) {
 //     module.hot.accept();
