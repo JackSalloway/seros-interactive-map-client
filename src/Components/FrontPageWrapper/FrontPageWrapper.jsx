39213910698bed38c8492251a85015feeae982cd
Jack Salloway <jacksalloway@hotmail.com>
2023-04-06 12:59:39 +0100

    FrontPageWrapper component changes

--- src/Components/FrontPageWrapper/FrontPageWrapper.jsx
+++ src/Components/FrontPageWrapper/FrontPageWrapper.jsx
@@ -7,9 +7,8 @@ const FrontPageWrapper = ({ campaign }) => {
 
     return (
         <div id="journal-front-page-wrapper">
-            <div id="journal-front-page-title">
-                <h1>{he.decode(campaign.campaign.name)}</h1>
-            </div>
+            <h2>{he.decode(campaign.campaign.name)}</h2>
+            <p>Select a tab or location to begin!</p>
         </div>
     );
 };
