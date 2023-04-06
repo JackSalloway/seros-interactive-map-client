b564688f288bcbbe9498499f9fd4ff3c3698fd18
Jack Salloway <jacksalloway@hotmail.com>
2023-04-06 14:54:02 +0100

    Remove console.log

--- src/Components/FrontPageWrapper/FrontPageWrapper.jsx
+++ src/Components/FrontPageWrapper/FrontPageWrapper.jsx
@@ -3,8 +3,6 @@ import he from "he";
 import "./FrontPageWrapper.css";
 
 const FrontPageWrapper = ({ campaign }) => {
-    console.log(campaign);
-
     return (
         <div id="journal-front-page-wrapper">
             <h2>{he.decode(campaign.campaign.name)}</h2>
