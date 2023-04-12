c280e80e87054a6dbe0719579544665a13dd0070
Jack Salloway <jacksalloway@hotmail.com>
2023-04-12 10:29:15 +0100

    Update campaign error element

--- src/Components/ErrorElements/CampaignError.jsx
+++ src/Components/ErrorElements/CampaignError.jsx
@@ -1,11 +1,31 @@
+import GenericError from "./GenericError";
+
 import "./CampaignError.css";
-import { Link } from "react-router-dom";
+import { Link, useMatches } from "react-router-dom";
 
 const CampaignError = () => {
+    let matches = useMatches();
+    let user = matches
+        .filter((match) => Boolean(match.handle?.user))
+        .map((match) => match.data)[0];
+
+    // Check if the user is actually logged in and send the generic error if they are not.
+    if (user.campaigns === undefined) {
+        return <GenericError />;
+    }
+
     return (
         <div id="campaign-error-element-wrapper">
             <h2>Oops!</h2>
-            <p>There was an error whilst trying to load this campaign...</p>
+            <p>
+                There was an error whilst trying to load this campaign.
+                <br />
+                It could be either of the following:
+            </p>
+            <ul>
+                <li>This campaign does not exist.</li>
+                <li>You are not a member of this campaign.</li>
+            </ul>
             <p>
                 Please return to the <Link to={"/dashboard"}>dashboard</Link>{" "}
                 and try selecting a campaign from there.
