dfb88e40519c7d55d14f26a5211ff1e5798bde38
Jack Salloway <jacksalloway@hotmail.com>
2023-06-09 11:33:58 +0100

    Remove unused imports

--- src/Components/LocationNotes/CombatInstances/CombatInstanceNotes.jsx
+++ src/Components/LocationNotes/CombatInstances/CombatInstanceNotes.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import he from "he";
 import dayjs from "dayjs";
