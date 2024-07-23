cf1545b121d9cc6c82e56d92d7e56b984f2753c3
Jack Salloway <jacksalloway@hotmail.com>
2024-07-23 15:24:48 +0100

    Implement more tests for ListSortIcon component

--- src/Components/ListSortIcon/ListSortIcon.test.jsx
+++ src/Components/ListSortIcon/ListSortIcon.test.jsx
@@ -20,9 +20,48 @@ test("setSortBy function gets called with appropriate parameter when icon is cli
     );
 
     // ACT
-    fireEvent.click(screen.getByTestId("list-sort-icon"));
+    const sortIcon = screen.getByTestId("list-sort-icon");
+    fireEvent.click(sortIcon);
 
     // ASSERT
-    expect(mockSetState).toHaveBeenCalledTimes(1);
-    expect(mockSetState).toHaveBeenCalledWith("revAlphabetical");
+    expect(mockSetState).toHaveBeenCalledTimes(1); // Check the function has been called 1 time
+    expect(mockSetState).toHaveBeenCalledWith("revAlphabetical"); // Check the parameter the function was called with
+});
+
+test("check class attribute is being assigned the right value when icon is not selected", () => {
+    // ARRANGE
+    render(
+        <ListSortIcon
+            iconName={"arrow-down-z-a"}
+            sortValue={"revAlphabetical"}
+            sortBy={"alphabetical"}
+            setSortBy={jest.fn()}
+        />
+    );
+
+    // ACT
+    const sortIcon = screen.getByTestId("list-sort-icon");
+    const className = sortIcon.getAttribute("class");
+
+    // ASSERT
+    expect(className).not.toContain("list-sort-icon-selected");
+});
+
+test("check class attribute is being assigned the right value when icon is selected", () => {
+    // ARRANGE
+    render(
+        <ListSortIcon
+            iconName={"arrow-down-z-a"}
+            sortValue={"revAlphabetical"}
+            sortBy={"revAlphabetical"}
+            setSortBy={jest.fn()}
+        />
+    );
+
+    // ACT
+    const sortIcon = screen.getByTestId("list-sort-icon");
+    const className = sortIcon.getAttribute("class");
+
+    // ASSERT
+    expect(className).toContain("list-sort-icon-selected");
 });
