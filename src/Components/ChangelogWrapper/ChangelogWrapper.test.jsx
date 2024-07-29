bdcfc6aa02cbe571725b2eaa34386ebe303805ed
Jack Salloway <jacksalloway@hotmail.com>
2024-07-29 14:00:46 +0100

    Create test for ChangelogWrapper component

---
+++ src/Components/ChangelogWrapper/ChangelogWrapper.test.jsx
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom";
+import { screen, render, fireEvent } from "@testing-library/react";
+import ChangelogWrapper from "./ChangelogWrapper";
+
+// FonteAwesome icon imports
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronDown);
+
+test("header element renders correctly and its content is the value of the title prop", async () => {
+    //ARRANGE
+    render(<ChangelogWrapper title={"Fake Title"} changelog={[]} />);
+
+    // ACT
+    const heading = await screen.findByRole("heading", { level: 3 });
+
+    // ASSERT
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Fake Title");
+});
