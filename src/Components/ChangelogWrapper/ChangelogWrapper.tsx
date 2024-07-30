37716e26c8ac138ef5df7210f1d3dbc5b2edd2ed
Jack Salloway <jacksalloway@hotmail.com>
2024-07-30 14:25:32 +0100

    Handle changelog array being empty

--- src/Components/ChangelogWrapper/ChangelogWrapper.tsx
+++ src/Components/ChangelogWrapper/ChangelogWrapper.tsx
@@ -20,6 +20,21 @@ const ChangelogWrapper: React.FC<ChangelogWrapperProps> = (props) => {
 
     const [selected, setSelected] = useState<boolean>(false); // State for opening/closing list
 
+    // Return the list of changelog entries or an element telling the user that no entries exist
+    const renderContent = () => {
+        return changelog.length > 0 ? (
+            <div className="list-content">
+                {changelog?.reverse().map((item) => {
+                    return <ChangelogItem key={item.id} item={item} />;
+                })}
+            </div>
+        ) : (
+            <div className="list-content">
+                <p>No entries found.</p>
+            </div>
+        );
+    };
+
     return (
         <div
             className={`changelog-wrapper ${
@@ -30,13 +45,7 @@ const ChangelogWrapper: React.FC<ChangelogWrapperProps> = (props) => {
                 <h3>{title}</h3>
                 <FaChevronIcon open={selected} toggleOpen={setSelected} />
             </div>
-            {selected === true ? (
-                <div className="list-content">
-                    {changelog?.reverse().map((item) => {
-                        return <ChangelogItem key={item.id} item={item} />;
-                    })}
-                </div>
-            ) : null}
+            {selected === true ? renderContent() : null}
         </div>
     );
 };
