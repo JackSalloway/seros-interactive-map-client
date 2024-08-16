c34be702d138dc26c6e7e6a5800cbd11f5a3bf57
Jack Salloway <jacksalloway@hotmail.com>
2024-08-16 16:21:07 +0100

    feat (edit icon): add edit icon component

---
+++ src/Components/FaEditIcon/FaEditIcon.tsx
@@ -0,0 +1,34 @@
+import React, { SetStateAction } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Type imports
+import type {
+    Location,
+    Sublocation,
+    NPC,
+    Quest,
+    CombatInstance,
+} from "../../types";
+
+interface FaEditIconProps {
+    item: Location | Sublocation | NPC | Quest | CombatInstance;
+    editing: null | Sublocation;
+    setEditing: React.Dispatch<SetStateAction<null | Sublocation>>;
+}
+
+const FaEditIcon: React.FC<FaEditIconProps> = (props) => {
+    return (
+        <div>
+            <FontAwesomeIcon
+                icon="pencil"
+                className="edit-button"
+                onClick={() => {
+                    window.alert("Edit functionality is not implemented yet!");
+                    props.setEditing(props.item);
+                }}
+            />
+        </div>
+    );
+};
+
+export default FaEditIcon;
